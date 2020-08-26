4f7d246e2027acd0f927f500b08c6d7c73109351
hf018 <hf018@hdm-stuttgart.de>
2020-08-26 19:45:20 +0200

    log analyzer for country and town

--- ch05_webapp/search4.py
+++ ch05_webapp/search4.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 
 app = Flask(__name__)
+
+
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 
 @app.route('/search4', methods=['POST'])
@@ -8,6 +13,7 @@ def do_search() -> 'html':
     phrase = request.form["phrase"]
     letters = request.form["letters"]
     result = str(lettersCheck(phrase, letters))
+    log_request(request, result)
     return render_template('results.html',
                            the_title="Ergebnisse ihrer Suche!",
                            the_phrase=phrase,
@@ -15,6 +21,7 @@ def do_search() -> 'html':
                            the_results=result)
 
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html',
@@ -25,9 +32,9 @@ def lettersCheck(phrase: str, letters: str = 'aeiou') -> set:
     return set(letters).intersection(set(phrase))  # Check for intersection
 
 
-def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(req, res, file=log)
+@app.route('/viewlog')
+def log_page() -> str:
+    return escape(open('vsearch.log').read())
 
 
 if __name__ == '__main__':
